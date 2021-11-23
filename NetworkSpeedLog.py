51775922804f236beeb18494030b53018b3e5834
Kristin Branson <bransonk@janelia.hhmi.org>
2021-11-23 14:53:29 -0500

    Now works on mac?

--- NetworkSpeedLog.py
+++ NetworkSpeedLog.py
@@ -1,3 +1,7 @@
+# Requires Python3
+# Makes a file in your home directory called NetworkSpeedLog<timestamp>.csv
+# with ping timing information
+
 import subprocess
 import re
 from pathlib import Path
@@ -6,9 +10,12 @@ import os
 import os.path
 import time
 import sys
+import platform
 
 def main():
 
+    osname = platform.system
+    
     timecode = '%Y%m%dT%H%M%S.%f'
     npings = 10
     timeout = 5 # s
@@ -21,13 +28,14 @@ def main():
     outfile = os.path.join(home,f'NetworkSpeedLog{starttime}.csv')
     fid = open(outfile,'w')
 
+    fid.write(f'timestamp,cmdfailure,parsefailure,received(%),lost(%),average(ms)\n')
     # windows and linux have different arguments to ping
-    if os.name == 'nt':
-        fid.write(f'timestamp,cmdfailure,parsefailure,received(%),lost(%),average(ms)\n')
+    if osname == 'Windows':
         cmd = ['ping','-n',str(npings),'-w',str(1000*timeout),host]
+    elif osname == 'Darwin':
+        cmd = ['ping','-c',str(npings),'-W',str(1000*timeout),host]
     else:
-        fid.write(f'timestamp,cmdfailure,parsefailure,received(%),lost(%),average(ms),total(ms)\n')
-        cmd = ['ping','-c',str(npings),'-w',str(timeout*npings),host]
+        cmd = ['ping','-c',str(npings),'-W',str(timeout*npings),host]
 
 
     while True:
@@ -39,7 +47,6 @@ def main():
         received = 0
         lost = 0
         average = float('nan')
-        total = float('nan')
         if res.returncode == 0:
             cmdfailure = 0
 
@@ -52,18 +59,16 @@ def main():
                     received = float(m.groups()[0])/float(npings)*100.
                     lost = float(m.groups()[1])/float(npings)*100.
                     average = float(m.groups()[2])
-                fid.write(f'{cmdtime},{cmdfailure},{parsefailure},{received},{lost},{average}\n')
             else:
-                m = re.search(r' (\d+) received.* (\d+)% packet loss, time ([\d\.]+)ms.*mdev = [^/]*/([^/]*)/', res.stdout.decode("utf-8"), re.DOTALL)
+                m = re.search(r' (\d+) .*received.* ([\d\.]+)% packet loss.*dev = [^/]*/([^/]*)/', res.stdout.decode("utf-8"), re.DOTALL)
                 if m is None:
                     parsefailure = 1
                 else:
                     received = float(m.groups()[0])/float(npings)*100.
                     lost = float(m.groups()[1])
-                    total = float(m.groups()[2])
-                    average = float(m.groups()[3])
+                    average = float(m.groups()[2])
 
-                fid.write(f'{cmdtime},{cmdfailure},{parsefailure},{received},{lost},{average},{total}\n')
+        fid.write(f'{cmdtime},{cmdfailure},{parsefailure},{received},{lost},{average}\n')
         fid.flush()
         time.sleep(sleeptime)
 
